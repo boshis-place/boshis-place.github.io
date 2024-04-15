60a3a54884bd6a61e4590dcd7660179b81fe9e5b
ty cobb <bin@tycobb.club>
2024-04-15 14:05:58 -0400

    use rand style var instead of css class

--- .eleventy.js
+++ .eleventy.js
@@ -128,20 +128,28 @@ module.exports = function (config) {
 
   config.addFilter("named", named)
 
-  // -- filters/misc
+  // -- filters/rand
   /// sample a random number up to a maximum (exclusive)
-  function rand(max) {
+  function rand(max = 1) {
     return Math.random() * max
   }
 
   config.addFilter("rand", rand)
 
   /// sample a random integer up to a maximum (exclusive)
-  function randi(max) {
+  function randInt(max) {
     return Math.floor(rand(max))
   }
 
-  config.addFilter("randi", randi)
+  config.addFilter("randint", randInt)
+
+  // -- shortcodes --
+  /// sample a random number as a css variable
+  function randVar() {
+    return `--rand: ${rand()}`
+  }
+
+  config.addShortcode("randvar", randVar)
 
   // -- output --
   return {
