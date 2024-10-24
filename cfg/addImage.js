3e63acd548fa0d5c63884eb5e8d323ca1691dfa7
ty cobb <bin@tycobb.club>
2024-10-24 06:17:24 -0400

    make img shortcode extension independent

--- cfg/addImage.js
+++ cfg/addImage.js
@@ -1,9 +1,46 @@
 const Image = require("@11ty/eleventy-img")
-const { join } = require("path")
+const { join, extname } = require("path")
+const { readdir } = require("fs/promises")
 
 module.exports = function (config, srcDir, dstDir) {
   // -- constants --
   const imgDir = join(srcDir, "img")
+
+  // build a map of img prefix to path, e.g. "img/example/image" => "img/example/image.png"
+  const imgPathByPrefix = readdir(imgDir, {
+    recursive: true,
+  }).then((children) => {
+    const map = new Map()
+
+    for (const child of children) {
+      const path = join(imgDir, child)
+
+      const prefix = findImagePrefix(path)
+      if (prefix != null) {
+        map.set(prefix, path)
+      }
+    }
+
+    return map
+  })
+
+  // find the extension-less prefix for a path
+  function findImagePrefix(path) {
+    const ext = extname(path)
+    if (ext === "") {
+      return null
+    }
+
+    return path.slice(0, -ext.length)
+  }
+
+  // find the image path for the src path. the src path may have no extension. if it
+  // has an extension, the extension is ignored.
+  async function findImagePath(src) {
+    const prefix = findImagePrefix(src) ?? src
+    const path = (await imgPathByPrefix).get(prefix)
+    return path
+  }
 
   // -- shortcodes --
   /// img shortcode for rendering optimized images
@@ -22,6 +59,7 @@ module.exports = function (config, srcDir, dstDir) {
     const isRemoteUrl = Image.Util.isRemoteUrl(src)
     if (!isRemoteUrl) {
       src = join(imgDir, src)
+      src = await findImagePath(src)
     }
 
     const meta = await Image(src, {
